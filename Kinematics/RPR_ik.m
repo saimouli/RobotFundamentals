5a4312c12321903f1cd984c01e062cb667556939
ksaimouli <ksaimouli@gmail.com>
2017-08-29 11:20:49 -0400

    updated

--- Kinematics/RPR_ik.m
+++ Kinematics/RPR_ik.m
@@ -6,14 +6,22 @@ function [ ik_sol ] = RPR_ik( x, y, z, R )
 %    the end effector position and orientation.
 
     %% YOUR CODE GOES HERE
+    %ik_sol = ones(1, 3);
     
+    %sin(theta1)/cos(theta1) = tan(theta1) (R21,R11)
+    theta1= atan2(R(2,1),R(1,1));
     
-    ik_sol = ones(1, 3);
-
+    theta3= atan2(R(3,3),-R(3,2)); %=+/-
+    
+    d2 = -(z-5*sin(theta3)-10)/cos(pi/4); %z of P03
+    %d2=(y-5*cos(theta3)*cos(theta1))/(sin(pi/4)*cos(theta1));
+    ik_sol = [theta1 d2 theta3];
+    
+        
 end
 
 
 % Test case
-%[pos, R] = RPR_fk(pi,-10,pi/2)
-%ik_sol = RPR_ik( pos(1), pos(2), pos(3), R );
-%[pos_ik, R_ik] = RPR_fk(ik_sol(1),ik_sol(2),ik_sol(3))
+% [pos, R] = RPR_fk(0,5,0)
+% ik_sol = RPR_ik( pos(4,1), pos(4,2), pos(4,3), R );
+% [pos_ik, R_ik] = RPR_fk(ik_sol(1),ik_sol(2),ik_sol(3))
